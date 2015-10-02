f11b2258909a12e0d8466ed409c110e099b2ead0
italogar6 <italogar6@hotmail.com>
2015-10-02 18:48:11 +0100

    main nuevo

--- Practica1/Cruzar.m
+++ Practica1/Cruzar.m
@@ -1,10 +1,10 @@
-function [popHijo] = Cruzar (pop, numSize)
+function [popHijo] = Cruzar (pop)
 
     %Paso 1 -->Guardar la élite, los 5 primeros
 	popHijo = [];
 	popHijo = [popHijo; pop(1:5,:);
 
 	%Paso 2 --> coger el primero y mezclarlo con el segundo, y así hasta la mitad de la población.
-	
+	for 
 	%Paso 3 --> coger y cruzar randoms hasta llegar a numSize
 end
--- Practica1/main.m
+++ Practica1/main.m
@@ -1,35 +1,21 @@
 function [fin] = main(I , popSize)
     fin = 0;
     
-<<<<<<< HEAD
     pop = Inicializa_poblacion(popSize);
     pop = OrdenaFitness(pop, I);
-=======
-    pop = Inicializa_poblacion(100);
-    pop = OrdenaFitness(pop, i);
->>>>>>> origin/master
     
     while (EvaluarEstado(pop)) %si el primero cumple las condiciones, exit while
         pop = Seleccionar(pop); %seleccionar 2/3 mejores
         Mostrarpop.MostrarPadre(pop);
         
-<<<<<<< HEAD
-        popHijo = Cruzar(pop, popSize); %hacer cruce de cromosomas
+        popHijo = Cruzar(pop); %hacer cruce de cromosomas
         MostrarPob.MostrarHijos(pop);
         
         popHijo = Mutar(popHijo); %escoger uno aleatorio y cambiarle un bit
         MostrarPob.MostrarMutado(cromosomaMutado);
         
         OrdenaFitness(popHijo, I); %ordenar la nueva población
-=======
-        popHijo = Cruzar(pop); %hacer cruce de cromosomas
-        Mostrarpop.MostrarHijos(pop);
-        
-        cromosomaMutado = Mutar(popHijo); %escoger uno aleatorio y cambiarle un bit
-        Mostrarpop.MostrarMutado(cromosomaMutado);
-        
-        OrdenaFitness(popHijo, I); %ordenar la nueva poplación
->>>>>>> origin/master
+
         pop = popHijo;
     end
     
