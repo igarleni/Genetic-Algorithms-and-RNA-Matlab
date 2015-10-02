ad21f759c7e1090d5bdbb915fa0de39ffca2ecba
italogar6 <italogar6@hotmail.com>
2015-10-02 18:46:15 +0100

    cruzar

--- Practica1/Cruzar.m
+++ Practica1/Cruzar.m
@@ -1,6 +1,10 @@
 function [popHijo] = Cruzar (pop, numSize)
-    %Guardar la élite --> los 5 primeros
-    %realizar cruce hasta llegar a numSize
-		%Cruce1 --> coger el primero y mezclarlo con el segundo, y así hasta la mitad de la población.
-		%coger y cruzar randoms hasta llegar a numSize
+
+    %Paso 1 -->Guardar la élite, los 5 primeros
+	popHijo = [];
+	popHijo = [popHijo; pop(1:5,:);
+
+	%Paso 2 --> coger el primero y mezclarlo con el segundo, y así hasta la mitad de la población.
+	
+	%Paso 3 --> coger y cruzar randoms hasta llegar a numSize
 end
