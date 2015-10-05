2d38c2695bb7be0a721f8f321a5855db82653e2a
CarlaCabestrero <carlacabes94@gmail.com>
2015-10-05 20:35:53 +0100

    mainCarla

---
+++ Practica1/mainCarla.m
@@ -0,0 +1,32 @@
+function [fin] = main(imagen , maxGen)
+    fin = 0;
+    b1=edge(img,'sobel','vertical');
+    b2=edge(img,'sobel','horizontal');
+	I = imadd(b1,b2);
+
+    pop = Inicializa_poblacion(100);
+    pop = OrdenaFitness(pop, I);
+    probMutacion = 0.07;
+    
+    numeroGeneraciones = 1;
+    MostrarPop(pop, I);
+
+    while (EvaluarEstado(pop, maxGen)) %si el primero cumple las condiciones o si hay muchas generaciones, exit while
+        pop = pop(1:70,:); %seleccionar 2/3 mejores
+        
+        popHijo = Cruzar(pop); %hacer cruce de cromosomas
+        
+        popHijo = Mutar(popHijo, probMutacion); %escoger uno aleatorio y cambiarle un bit
+        popHijo = [popHijo; Inicializa_poblacion(25)]; %Se rellenan los 25 que faltan para llegar a 100
+        
+        OrdenaFitness(popHijo, I); %ordenar la nueva población
+
+        pop = popHijo;
+        
+        numeroGeneraciones = numeroGeneraciones + 1;
+        MostrarPop(pop, I, numeroGeneraciones);
+        
+    end
+    MostrarCandidato(pop, I);
+    
+end
